90cefcbe349f6500cede80f4f824d3a871182772
muhammad12344321 <usuf38010@gmail.com>
2025-05-09 12:58:18 +0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./Layout/Login/Login";
-import Main from "./Layout/Main/index";
+import Main from "./Layout/Main/index"; // макет с Sidebar и <Outlet />
+import Mentors from "./modules/Mentors";
+import Courses from "./modules/Courses";
+import Dashboard from "./modules/Dashboard"; // пример
+
 import "./assets/scss/main.scss";
 
-export default function Layout() {
+export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Main />} />
+      <Route path="/" element={<Main />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="mentors" element={<Mentors />} />
+        <Route path="courses" element={<Courses />} />
+      </Route>
     </Routes>
   );
 }
